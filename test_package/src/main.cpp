87f5245540c52e9eea5239e3a90b9ffe054b79c3
rtam-leica <161450412+rtam-leica@users.noreply.github.com>
2025-11-05 08:23:44 +0100

    Missing .ipp files from Conan package - fixed (#2)

--- test_package/src/main.cpp
+++ test_package/src/main.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 #include <saam/version.hpp>
+#include <saam/safe_ref.hpp>
 
 #include <iostream>
 
@@ -10,5 +11,10 @@ int main()
 {
     using namespace saam;
     std::cout << "saam version:" << version::major() << '.' << version::minor() << '.'<< version::patch() << std::endl;
+
+    saam::var<int> smart_variable{42};
+    *smart_variable.borrow() = 22;
+    std::cout << *smart_variable.borrow() << std::endl;
+
     return 0;
 }
